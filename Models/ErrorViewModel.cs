d89fd5bcaa0f22bc21ed0da1f85be55bbf253b26
student <student@DESKTOP-M5H11TP>
2022-12-09 07:49:40 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
